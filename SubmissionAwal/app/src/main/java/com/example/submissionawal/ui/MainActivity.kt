a733798825fe97b1d7599fc8443e3e8ff7b5a5ff
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-03-19 22:05:27 +0800

    main view model

--- SubmissionAwal/app/src/main/java/com/example/submissionawal/ui/MainActivity.kt
+++ SubmissionAwal/app/src/main/java/com/example/submissionawal/ui/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.submissionawal.ui
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.size
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.submissionawal.R
-import com.example.submissionawal.data.response.GithubResponse
 import com.example.submissionawal.data.response.ItemsItem
-import com.example.submissionawal.data.retrofit.ApiConfig
 import com.example.submissionawal.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +29,14 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        mainViewModel.listUser.observe(this){
+            setUserData(it)
+        }
+
+        mainViewModel.isLoading.observe(this){
+            showLoading(it)
+        }
+
         val layoutManager = LinearLayoutManager(this)
         binding.rvUser.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
@@ -44,36 +46,11 @@ class MainActivity : AppCompatActivity() {
             searchView.setupWithSearchBar(searchBar)
             searchView.editText.setOnEditorActionListener {textView, actionId, event ->
                 searchBar.setText(searchView.text)
-                findUser(searchBar.text.toString())
+                mainViewModel.findUser(searchBar.text.toString())
                 searchView.hide()
                 false
             }
         }
-    }
-
-    private fun findUser(query: String){
-        showLoading(true)
-        val client = ApiConfig.getApiService().getUser(query)
-        client.enqueue(object : Callback<GithubResponse> {
-            override fun onResponse(
-                call: Call<GithubResponse>,
-                response: Response<GithubResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setUserData(responseBody.items)
-                    }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
     }
 
     private fun setUserData(user: List<ItemsItem>) {
